f7fb33d36d11257d9f1515b205edbd6d172b933a
aethletic <gorshkov34@gmail.com>
2020-03-28 00:29:53 +0400

    [feat] рефакторинг

--- index.php
+++ index.php
@@ -1,29 +1,20 @@
 <?php
 
+use Aethletic\Telegram\Bot;
 use Aethletic\Telegram\File;
 
 require __DIR__ . '/bot/bootstrap.php';
 
-$rick->callback('{default}', function() use ($rick) {
-    $rick->notify('Ошибка: колбэк не определён!');
-});
+$rick->callback('{default}', 'Callback::default');
+$rick->callback('example_callback_data', 'Callback::example');
 
-$rick->callback('example_callback_data', function() use ($rick) {
-    $rick->notify('Этот колбэк определён в файле index.php');
-});
-
-$rick->hear('{default}', function() use ($rick) {
-    $rick->say('К сожалению, сообщение не распознано.');
-});
-
-$rick->hear(['/start', 'Назад'], function() use ($rick, $user) {
-    $rick->say("Привет, {$user['fullname']}!", $rick->keyboard('example'));
-});
-
-$rick->hear('Картинка', 'Example::picture');
-$rick->hear('Файл', 'Example::file');
-$rick->hear('Клавиатура', 'Example::keyboard');
+$rick->hear('{default}', 'Event::default');
+$rick->hear(['/start', '/keyboard', 'Назад'], 'Event::start');
 $rick->hear('Inline-клавиатура', 'Example::inline');
 $rick->hear('Спрятать клавиатуру', 'Example::hide');
+$rick->hear('Клавиатура', 'Example::keyboard');
+$rick->hear('Анимация', 'Example::animation');
+$rick->hear('Картинка', 'Example::picture');
+$rick->hear('Файл', 'Example::file');
 
 $rick->run();
